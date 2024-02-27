d60c54e43cb477c27421e789e62438de0e0acf98
conrad mugabe <conradmugabe@gmail.com>
2024-02-27 16:18:58 +0300

    feat: added main logic for the app

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	companynames "github.com/conradmugabe/simple-web-scrapper-go/src"
+)
+
+const (
+	fileName  = "companies.txt"
+	directory = "."
+)
+
+func main() {
+	fs := os.DirFS(directory)
+	companies, err := companynames.CompanyNamesFromTextFile(fs, fileName)
+	if err != nil {
+		panic(err)
+	}
+	for _, company := range companies {
+		url, _ := companynames.ConstructURLWithParams("https://www.google.com/search", map[string]string{"q": company.Name})
+		body, _ := companynames.GetWebsiteContent(url)
+		urls := companynames.ExtractURLs(body)
+		fmt.Println(urls)
+		// for _, url := range urls {
+		// 	println(url)
+		// }
+	}
 }

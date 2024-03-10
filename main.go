4bef7d1156c2eb246f6aebd52390b7485fcb0e2c
conrad mugabe <conradmugabe@gmail.com>
2024-03-11 01:53:05 +0300

    feat: added logrus for logging

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	companynames "github.com/conradmugabe/simple-web-scrapper-go/src"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -21,14 +21,18 @@ func main() {
 	}
 
 	for _, company := range companies {
+		log.Info("Constructing google search url for ", company.Name)
 		url, _ := companynames.ConstructURLWithParams("https://www.google.com/search", map[string]string{"q": company.Name})
+		log.Info("Making a google search for ", company.Name)
 		body, _ := companynames.GetWebsiteContent(url)
+		log.Info("Extracting facebook url for ", company.Name)
 		urls := companynames.ExtractURLs(body)
 		companyFacebookURL := companynames.GetAllFacebookLink(urls)
 		companyFacebookAboutPageURL := companynames.AddAboutLinkSuffix(companyFacebookURL)
+		log.Info("Extracting email for ", company.Name, " from ", companyFacebookAboutPageURL)
 		aboutPageBody, _ := companynames.GetWebsiteContent(companyFacebookAboutPageURL)
 		emails := companynames.ExtractEmailsFromText(aboutPageBody)
 		company.Email = companynames.GetFirstEntryInList(emails)
-		fmt.Println(company)
+		log.Info("Email for ", company.Name, " is ", company.Email)
 	}
 }

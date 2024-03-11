98ab57517da2fe822f799ea53dfe7d755e3b9702
conrad mugabe <conradmugabe@gmail.com>
2024-03-11 03:36:06 +0300

    feat: added script to save company data to file system

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 )
 
 const (
-	fileName  = "companies.txt"
-	directory = "."
+	fileName   = "companies.txt"
+	directory  = "."
+	outputFile = "companies_with_emails.txt"
 )
 
 func main() {
@@ -20,11 +21,14 @@ func main() {
 		panic(err)
 	}
 
+	companiesWithEmails := make([]companynames.Company, 0)
+
 	for _, company := range companies {
 		log.Info("Constructing google search url for ", company.Name)
 		url, _ := companynames.ConstructURLWithParams("https://www.google.com/search", map[string]string{"q": company.Name})
 		log.Info("Making a google search for ", company.Name)
 		body, _ := companynames.GetWebsiteContent(url)
+
 		log.Info("Extracting facebook url for ", company.Name)
 		urls := companynames.ExtractURLs(body)
 		companyFacebookURL := companynames.GetAllFacebookLink(urls)
@@ -32,7 +36,17 @@ func main() {
 		log.Info("Extracting email for ", company.Name, " from ", companyFacebookAboutPageURL)
 		aboutPageBody, _ := companynames.GetWebsiteContent(companyFacebookAboutPageURL)
 		emails := companynames.ExtractEmailsFromText(aboutPageBody)
-		company.Email = companynames.GetFirstEntryInList(emails)
-		log.Info("Email for ", company.Name, " is ", company.Email)
+		email := companynames.GetFirstEntryInList(emails)
+		log.Info("Email for ", company.Name, " is ", email)
+		companiesWithEmails = append(companiesWithEmails, companynames.Company{Name: company.Name, Email: email})
 	}
+
+	log.Info("Saving company details to file...")
+
+	Err := companynames.SaveToFile(outputFile, companiesWithEmails)
+	if Err != nil {
+		log.Error("Failed to save company details to file: ", Err)
+	}
+
+	log.Info("Completed!")
 }
